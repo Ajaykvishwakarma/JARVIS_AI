1c7413076bd66100c56e8bf52af225b060b86456
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-05-14 15:16:20 +0530

    JARVIS

--- FRIDAY/app.js
+++ FRIDAY/app.js
@@ -20,8 +20,6 @@ recognition.onstart = function () {
     console.log("vr active");
 }
 
-// sr continues
-recognition.continuous = true;
 
 // sr result
 recognition.onresult = function (event) {
@@ -40,6 +38,10 @@ recognition.onend = function () {
     console.log("vr deactive");
 }
 
+// sr continues
+// recognition.continuous = true;
+
+
 startBtn.addEventListener("click", () => {
     recognition.start()
 })
@@ -55,9 +57,9 @@ function readOut(message) {
     // default voice --- jarvis
     // change voices...     to check how many voices u have, open browser console and paste -> speechSynthesis.getVoices()
 
-    const allVoices = speechSynthesis.getVoices()
+    // const allVoices = speechSynthesis.getVoices()
     speech.text = message;
-    speech.voice = allVoices[2];  // 2 is the index of voices (Zira's voice)
+    // speech.voice = allVoices[2];  // 2 is the index of voices (Zira's voice)
     speech.volume  = 1;
     window.speechSynthesis.speak(speech)
     // console.log("speaking");
---
+++ JARVIS_4/app.js
@@ -0,0 +1,11 @@
+const microphoneButton = document.getElementById('microphoneButton');
+
+microphoneButton.addEventListener('click',function(){
+    window.open(
+        'http://127.0.0.1:5555/JARVIS_4/Jarvis-in-Javascript-main/Jarvis-in-Javascript-main/mic/index1.html',
+        "",
+        "width=280px,height=280px"
+    );
+});
+
+
